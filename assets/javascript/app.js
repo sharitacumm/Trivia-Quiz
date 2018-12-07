dce3364a825c19311bdea6b9ae21301ab32c9d1a
sharitacumm <emandraine@yahoo.com>
2018-12-07 18:38:52 -0500

    Linked javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,111 @@
+//Create an array of the questions//
+
+var myQuestions = [
+    {
+        question: "Which color is a shade of Green?",
+        answers: {
+        a: "fuscia",
+        b: "charcoal",
+        c: "charteuse",
+        d: "magenta",
+        e: "carob",
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which color is a shade of Brown?",
+        answers: {
+        a: "Sienna",
+        b: "lapis",
+        c: "ash",
+        d: "orchid",
+        e: "coconut",
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which color is a shade of Red?",
+        answers: {
+        a: "merigold",
+        b: "cobalt",
+        c: "fern",
+        d: "crimson",
+        e: "taupe",
+        },
+        correctAnswer: "d"
+    },
+      {
+        question: "Which color is a shade of Grey?",
+        answers: {
+            a: "iris",
+            b: "pewter",
+            c: "chiffon",
+            d: "amaranth",
+            e: "fuscia",
+        },
+        correctAnswer: "b"
+    },
+    //Need to add closing baracket for object//
+    //     {
+    //     question: "Which color is a shade of Purple?",
+    //     answers: {
+    //     a: "byzanthium"
+    //     b: "lava"
+    //     c: "shamrock"
+    //     d: "daffodil"
+    //     e: "medallion"
+    //     },
+    //     correctAnswer: "a"
+    //   {
+    //     question: "Which color is a shade of White?",
+    //     answers: {
+    //         a: "mauve"
+    //         b: "aegean"
+    //         c: "currant"
+    //         d: "hibiscus"
+    //         e: "alabaster"
+    //     },
+    //     correctAnswer: "e"
+    //       {
+    //     question: "Which color is a shade of Yellow?",
+    //     answers: {
+    //         a: "amethyst"
+    //             b: "mahogany"
+    //             c: "umber"
+    //             d: "tuscany"
+    //             e: "wistaria"
+    //     },
+    //     correctAnswer: "d"
+    //           {
+    //     question: "Which color is a shade of Blue?",
+    //     answers: {
+    //         a: "azure"
+    //                 b: "pine"
+    //                 c: "vermillion"
+    //                 d: "pitch"
+    //                 e: "ecrue"
+    //     },
+    //     correctAnswer: "a"
+    //               {
+    //     question: "Which color is a shade of Orange?",
+    //     answers: {
+    //         a: "cerulean"
+    //                 b: "anchor"
+    //                 c: "monarch"
+    //                 d: "emerald"
+    //                 e: "shadow"
+    //     },
+    //     correctAnswer: "c"   
+    //               {
+    //     question: "Which color is a shade of Black?",
+    //     answers: {
+    //         a: "ginger"
+    //                 b: "flaxen"
+    //                 c: "obsidian"
+    //                 d: "salmon"
+    //                 e: "seafoam"
+    //     },
+    //     correctAnswer: "c"
+    // }
+];
+console.log("myQuestions",myQuestions)
