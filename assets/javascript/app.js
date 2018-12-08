8d7a2f8bd305ee05c018d42e6f77faba2ffe2066
sharitacumm <emandraine@yahoo.com>
2018-12-08 12:34:31 -0500

    Made changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,7 +34,7 @@ var myQuestions = [
         },
         correctAnswer: "d"
     },
-      {
+    {
         question: "Which color is a shade of Grey?",
         answers: {
             a: "iris",
@@ -45,68 +45,72 @@ var myQuestions = [
         },
         correctAnswer: "b"
     },
-    //Need to add closing baracket for object//
-    //     {
-    //     question: "Which color is a shade of Purple?",
-    //     answers: {
-    //     a: "byzanthium"
-    //     b: "lava"
-    //     c: "shamrock"
-    //     d: "daffodil"
-    //     e: "medallion"
-    //     },
-    //     correctAnswer: "a"
-    //   {
-    //     question: "Which color is a shade of White?",
-    //     answers: {
-    //         a: "mauve"
-    //         b: "aegean"
-    //         c: "currant"
-    //         d: "hibiscus"
-    //         e: "alabaster"
-    //     },
-    //     correctAnswer: "e"
-    //       {
-    //     question: "Which color is a shade of Yellow?",
-    //     answers: {
-    //         a: "amethyst"
-    //             b: "mahogany"
-    //             c: "umber"
-    //             d: "tuscany"
-    //             e: "wistaria"
-    //     },
-    //     correctAnswer: "d"
-    //           {
-    //     question: "Which color is a shade of Blue?",
-    //     answers: {
-    //         a: "azure"
-    //                 b: "pine"
-    //                 c: "vermillion"
-    //                 d: "pitch"
-    //                 e: "ecrue"
-    //     },
-    //     correctAnswer: "a"
-    //               {
-    //     question: "Which color is a shade of Orange?",
-    //     answers: {
-    //         a: "cerulean"
-    //                 b: "anchor"
-    //                 c: "monarch"
-    //                 d: "emerald"
-    //                 e: "shadow"
-    //     },
-    //     correctAnswer: "c"   
-    //               {
-    //     question: "Which color is a shade of Black?",
-    //     answers: {
-    //         a: "ginger"
-    //                 b: "flaxen"
-    //                 c: "obsidian"
-    //                 d: "salmon"
-    //                 e: "seafoam"
-    //     },
-    //     correctAnswer: "c"
-    // }
+    {
+        question: "Which color is a shade of Purple?",
+        answers: {
+            a: "byzanthium",
+            b: "lava",
+            c: "shamrock",
+            d: "daffodil",
+            e: "medallion",
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which color is a shade of White?",
+        answers: {
+            a: "mauve",
+            b: "aegean",
+            c: "currant",
+            d: "hibiscus",
+            e: "alabaster",
+        },
+        correctAnswer: "e"
+    },
+    {
+        question: "Which color is a shade of Yellow?",
+        answers: {
+            a: "amethyst",
+            b: "mahogany",
+            c: "umber",
+            d: "tuscany",
+            e: "wistaria",
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Which color is a shade of Blue?",
+        answers: {
+            a: "azure",
+            b: "pine",
+            c: "vermillion",
+            d: "pitch",
+            e: "ecrue",
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which color is a shade of Orange?",
+        answers: {
+            a: "cerulean",
+            b: "anchor",
+            c: "monarch",
+            d: "emerald",
+            e: "shadow",
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which color is a shade of Black?",
+        answers: {
+            a: "ginger",
+            b: "flaxen",
+            c: "obsidian",
+            d: "salmon",
+            e: "seafoam",
+        },
+        correctAnswer: "c"
+    }
 ];
 console.log("myQuestions",myQuestions)
 //  
